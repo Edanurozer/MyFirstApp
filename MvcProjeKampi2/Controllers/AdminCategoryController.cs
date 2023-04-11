3086b901ef1e69070b6cbdd6398c000e5db7d093
Edanurozer <maviakyilmaz53@gmail.com>
2023-04-11 11:48:31 +0300

    Categori silme islemi yapıldı

--- MvcProjeKampi2/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi2/Controllers/AdminCategoryController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +14,45 @@ namespace MvcProjeKampi2.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
+
+        CategoryManeger cm = new CategoryManeger(new EfCategoryDal());
         public ActionResult Index()
         {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory( Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index"); // ındex aksiyonuna yönlendirecek eğer girilen değerler geçerliyse
+            }
+            else
+            {
+                foreach (var item in results.Errors)       
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+
+            return View();
+        }
+        
+        public ActionResult DeleteCategory(int id)
+        {
+            var categoryvalue = cm.GetByID(id);
+            cm.DeleteCategroy(categoryvalue);
+
+            return RedirectToAction("Index");
         }
     }
 }
