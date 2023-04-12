b7ce3130b773fde920e11c0533df734ff4d09c6f
Edanurozer <maviakyilmaz53@gmail.com>
2023-04-12 17:13:59 +0300

    ekleme işlemleri yapıldı

---
+++ MvcProjeKampi2/Controllers/WriterController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi2.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterMenager wm = new WriterMenager(new EfWriterDal());
+
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList(); //ekleme
+            return View(WriterValues);
+        }
+    }
+}
