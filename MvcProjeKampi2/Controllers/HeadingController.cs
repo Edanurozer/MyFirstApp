fe831612d1f0088e4351921b1e6555b63732d0f4
Edanurozer <maviakyilmaz53@gmail.com>
2023-05-15 16:06:47 +0300

    Açıklama kısmında düzeltme yapıldı

--- MvcProjeKampi2/Controllers/HeadingController.cs
+++ MvcProjeKampi2/Controllers/HeadingController.cs
@@ -29,7 +29,7 @@ namespace MvcProjeKampi2.Controllers
             List<SelectListItem> valuecategory = (from x in cm.GetList()
                                                   select new SelectListItem // yeni bir liste öğesini seçecek
                                                   { Text = x.CategoryName, Value = x.CategoryID.ToString() }).ToList(); // verileri dropdownlist aracı üzerinden listeleyecek
-            // Dropdown iki adet parametresi oalcak biri ValueNumber= Seçmiş olduğum değerin ID' si   DisplayNumber= Seçmiş olduğum değerin görünüm kısmı yani categıriadı olacak
+            // Dropdown iki adet parametresi olacak biri ValueNumber= Seçmiş olduğum değerin ID' si   DisplayNumber= Seçmiş olduğum değerin görünüm kısmı yani categıriadı olacak
             // ValueNumber ve DisplayNumber' ın controller' daki karşılığı Text ve Value. Text Displaynumver oluyor Value valueNumber oluyoor.
 
 
@@ -48,6 +48,10 @@ namespace MvcProjeKampi2.Controllers
             p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString()); // günün tarihini yazacak
             hm.HeadingAdd(p);
             return RedirectToAction("Index");
+        }
+        public ActionResult ContentByHeading()
+        {
+            return View();
         }
     }
 }
