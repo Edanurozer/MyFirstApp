c1f0da00d55a13899d47845354748e862cc593aa
Edanurozer <maviakyilmaz53@gmail.com>
2023-05-15 16:03:01 +0300

    Listeme İşlemi Content

---
+++ BusinessLayer/Abstrac/IContentService.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstrac
+{
+   public interface IContentService
+    {
+
+        List<Content> GetList();
+        List<Content> GetListByHeadingID(int id); // İD ye göre sbütün listeyi döndürecek
+        void ContentAdd(Content content); // Categoryadd metodu,  category' den category parametresi alacak 
+        Content GetByID(int id);
+        void DeleteContent(Content content);
+        void ContentUpdate(Content content);
+
+
+
+        
+    }
+}
