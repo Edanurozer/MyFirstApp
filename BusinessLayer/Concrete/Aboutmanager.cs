eca31ed6bf96f3c0f46f70b84d183af730b4b019
Edanurozer <maviakyilmaz53@gmail.com>
2023-05-23 16:18:05 +0300

    Hakkımda (About) yeni sınıf eklendi ve interface dahil edildi

---
+++ BusinessLayer/Concrete/Aboutmanager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstrac;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class AboutManager: IAboutService
+    {
+        IAbaoutDal _aboutDal;
+
+        public AboutManager(IAbaoutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void AboutAdd(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AboutUpdate(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAbout(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
