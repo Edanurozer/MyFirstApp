99754348a8600d285518643432d126e738bdf092
Edanurozer <maviakyilmaz53@gmail.com>
2023-05-31 10:15:49 +0300

    İletişim sınıfı oluşturuldu

---
+++ BusinessLayer/Abstrac/IContactService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstrac
+{
+   public interface IContactService
+    {
+        List<Contact> GetList();
+
+        void ContactAdd(Contact contact); // Categoryadd metodu,  category' den category parametresi alacak 
+        Contact GetByID(int id);
+        void DeleteContact(Contact contact);
+        void AboutUpdate(Contact contact);
+    }
+}
