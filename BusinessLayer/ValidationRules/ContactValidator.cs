bafd34a8e7a63a36d9b1f5e28037693c004e197e
Edanurozer <maviakyilmaz53@gmail.com>
2023-05-31 10:22:36 +0300

    Validator katmanında yeni sınıf oluşturuldu, kurallar yazıldı

---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class ContactValidator :  AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.UserMail).NotEmpty().WithMessage("Mail Adresini Boş Bırakmayınız");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage(" Konu Adını Boş Bıraknayınız");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı Adını Boş Bırakmayınız");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen En Az 3 Karakter Gişi Yapınız");
+            RuleFor(x => x.UserName).MinimumLength(3).WithMessage("Lütfen En Az 3 Karakter Gişi Yapınız");
+            RuleFor(x => x.Subject).MinimumLength(50).WithMessage("Lütfen En Az 50 Karakter Gişi Yapınız");
+
+        }
+    }
+}
