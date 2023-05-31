e4d256fe7bc4db2cf32413d0525bf91a5ba22ec5
Edanurozer <maviakyilmaz53@gmail.com>
2023-05-31 10:18:23 +0300

    Abstrac katmanı ile bağlantısı kuruldu, interface implement edildi ve methotların içi dolduruldu

---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstrac;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class ContactManager:IContactService
+    {
+        IContactDal _contactDal;
+
+        public ContactManager(IContactDal contactDal)
+        {
+            this._contactDal = contactDal;
+        }
+
+        public void AboutUpdate(Contact contact)
+        {
+            _contactDal.Update(contact);
+        }
+
+        public void ContactAdd(Contact contact)
+        {
+            _contactDal.Insert(contact);
+        }
+
+        public void DeleteContact(Contact contact)
+        {
+            _contactDal.Delete(contact);
+        }
+
+        public Contact GetByID(int id)
+        {
+            return _contactDal.Get(x => x.ContectID == id);
+        }
+
+        public List<Contact> GetList()
+        {
+            return _contactDal.List();
+        }
+    }
+}
