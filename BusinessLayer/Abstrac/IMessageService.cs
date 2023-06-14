59353f520552b424ee5ea010fa68ebaa4236fcf0
Edanurozer <maviakyilmaz53@gmail.com>
2023-06-14 09:55:02 +0300

    Mesaj interface oluşturuldu

---
+++ BusinessLayer/Abstrac/IMessageService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstrac
+{
+   public interface IMessageService
+    {
+        List<Message> GetList();
+
+        void MessageAdd(Message message); // Categoryadd metodu,  category' den category parametresi alacak 
+        Message GetByID(int id);
+        void DeleteMessage(Message message);
+        void MessageUpdate(Message messasge);
+    }
+}
