0cb72e61070e3cd77c253ea69c3956be6981b0b3
Edanurozer <maviakyilmaz53@gmail.com>
2023-06-14 09:56:00 +0300

    Manager sınıfı oluşturuldu, interface eklenip implament edildi.

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstrac;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+   public class MessageManager : IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public void DeleteMessage(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Message> GetList()
+        {
+            return _messageDal.List(x => x.ReciverMail == "admin@gmail.com");
+        }
+
+        public void MessageAdd(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageUpdate(Message messasge)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
